e0006d364476f61c5040e4b243085a0b394f3e77
Angel Garcia <angelgarciaweb@gmail.com>
2024-03-13 15:44:44 -0400

    Add save method to ProjectRepository

--- src/Projects/Infraestructure/ProjectRepository.php
+++ src/Projects/Infraestructure/ProjectRepository.php
@@ -3,7 +3,11 @@
 namespace Src\Projects\Infraestructure;
 
 use App\Models\Projects;
+use Src\Projects\Domain\ProjectDescription;
 use Src\Projects\Domain\ProjectName;
+use Illuminate\Support\Str;
+use Src\Projects\Domain\ProjectStatus;
+use Src\Projects\Domain\ProjectUser;
 
 class ProjectRepository
 {
@@ -18,5 +22,15 @@ class ProjectRepository
     public function search(ProjectName $name)
     {
         return $this->model->where('name', 'ilike', '%' . $name->value() . '%')->get();
+    }
+
+    public function save(ProjectName $name, ProjectDescription $description, ProjectUser $user, ProjectStatus $status)
+    {
+        $this->model->name = $name->value();
+        $this->model->description = $description->value();
+        $this->model->code = Str::uuid();
+        $this->model->status = $status->value();
+        $this->model->user_id = $user->value();
+        $this->model->save();
     }
 }
