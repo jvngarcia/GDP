7e18af4c91f40a79a6069d62f6ba429c38663029
Angel Garcia <angelgarciaweb@gmail.com>
2024-03-13 15:43:31 -0400

    Add description column to projects table

--- database/migrations/2024_03_01_182149_create_projects_table.php
+++ database/migrations/2024_03_01_182149_create_projects_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
             $table->foreignId('user_id')->constrained();
             $table->string('name');
+            $table->text('description');
             $table->text('code');
             $table->boolean('status')->default(true);
             $table->timestamps();
