7bc83ecc4bbb026a30aa8ac37d8465a0df1408c9
Angel Garcia <angelgarciaweb@gmail.com>
2024-03-13 15:43:19 -0400

    Add fillable fields and user relationship to Projects model

--- app/Models/Projects.php
+++ app/Models/Projects.php
@@ -8,4 +8,17 @@ use Illuminate\Database\Eloquent\Model;
 class Projects extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'code',
+        'description',
+        'status',
+        'user_id',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
