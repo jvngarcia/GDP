9ff7fa3ab18a4f99673a5c08676dc708c7851f16
Angel Garcia <angelgarciaweb@gmail.com>
2024-03-13 15:43:13 -0400

    Refactor render method in Index.php

--- app/Livewire/Projects/Index.php
+++ app/Livewire/Projects/Index.php
@@ -2,7 +2,6 @@
 
 namespace App\Livewire\Projects;
 
-use App\Models\Projects;
 use Livewire\Component;
 use Livewire\WithoutUrlPagination;
 use Livewire\WithPagination;
@@ -16,10 +15,16 @@ class Index extends Component
     public string $search = '';
     private ProjectFinder $finder;
 
-    public function render(ProjectFinder $finder)
+    public function boot(ProjectFinder $finder)
+    {
+        $this->finder = $finder;
+    }
+
+
+    public function render()
     {
         return view('livewire.projects.index', [
-            'projects' => $finder->__invoke($this->search),
+            'projects' => $this->finder->__invoke($this->search),
         ]);
     }
 }
