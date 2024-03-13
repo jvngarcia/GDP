32c584ed2da6b1b4395914c9d084604012a3e26a
Angel Garcia <angelgarciaweb@gmail.com>
2024-03-13 15:44:21 -0400

    Add ProjectStatus class to Projects\Domain

---
+++ src/Projects/Domain/ProjectStatus.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Src\Projects\Domain;
+
+class ProjectStatus
+{
+    private $value;
+
+    public function __construct($value)
+    {
+        $this->value = $value;
+    }
+
+    public function value()
+    {
+        return $this->value;
+    }
+}
