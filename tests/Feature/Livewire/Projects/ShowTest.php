cdd1bca2dd9f4582b5b7bfa252812cb4d3279935
Angel Garcia <angelgarciaweb@gmail.com>
2024-03-15 11:03:20 -0400

    Add ShowTest for Livewire Projects

---
+++ tests/Feature/Livewire/Projects/ShowTest.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Tests\Feature\Livewire\Projects;
+
+use App\Livewire\Projects\Show;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Livewire\Livewire;
+use Tests\TestCase;
+
+class ShowTest extends TestCase
+{
+    /** @test */
+    public function renders_successfully()
+    {
+        Livewire::test(Show::class)
+            ->assertStatus(200);
+    }
+}
